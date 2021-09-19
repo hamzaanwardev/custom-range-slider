f2bd42b1dc84388fdf947655a150b20e4356daa7
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-19 23:09:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ range.addEventListener('input', (e) => {
     const max = +e.target.max
     const min = +e.target.min
 
-    const left = value * (num_width / max) - num_label / 2 + scale(value, min, max, 10, -10);
+    const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10);
 
     label.style.left = `${left}px`
 
